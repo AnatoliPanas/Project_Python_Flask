ca67a51dafb7a0ff015d1df8d05c880f4a469d3c
Anatoli <PanasAA@web.de>
2025-03-10 14:30:00 +0100

    update

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "You are on the HOME page."
+
+@app.route('/user/', defaults={'name': 'Anatoli'})
+@app.route('/user/<string:name>')
+def get_name(name):
+    return f"Hello, {name}!"
+
+@app.route('/registrate', methods=['POST'])
+def post_request():
+    data = request.json
+    username = data['username']
+    password = data['password']
+    email = data['email']
+    return f"Username: {username}\nPassword: {"*" * len(password)}\nEmail: {email}"
+
+@app.route('/registrate2', methods=['POST'])
+def post_request_form():
+    username = request.form['username']
+    password = request.form['password']
+    email = request.form['email']
+    return f"Username: {username}\nPassword: {password}\nEmail: {email}"
+
+@app.route('/file/<path:file_path>/<age>')
+def get_file(file_path, age):
+    return f"MyPath: {file_path} {age}!"
 
 if __name__ == "__main__":
     app.run(debug=True)
